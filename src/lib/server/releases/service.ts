ca3b95f9dbce96aebb229ab921ae6f3624b7531c
Kilian Gärtner <contact@kilian-gaertner.dev>
2024-05-10 18:16:26 +0200

    add (api): copy file after upload to correct place

--- src/lib/server/releases/service.ts
+++ src/lib/server/releases/service.ts
@@ -1,4 +1,9 @@
+import { env } from '$env/dynamic/private';
 import { getDb } from '$lib/server/database/service';
+import { createWriteStream } from 'fs';
+import { mkdir } from 'fs/promises';
+import { dirname, join, resolve } from 'path';
+import { Readable } from 'stream';
 
 interface NewRelease {
 	environment: {
@@ -26,10 +31,28 @@ async function createRelease(newRelease: NewRelease): Promise<Release> {
 		type: newRelease.type
 	};
 	const newDocument = await getDb().insertAsync(document);
-	return {
+	const release = {
 		id: newDocument._id,
 		...document
 	};
+	await storeFile(release, newRelease.file);
+	return release;
+}
+
+async function storeFile(release: Release, file: File) {
+	const targetPath = getPathToFile(release);
+	await mkdir(dirname(targetPath), { recursive: true });
+
+	// The typing is wrong because it does not support generics in its typing - the function is working
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore ts 2551
+	const readable = Readable.fromWeb(file.stream());
+	readable.pipe(createWriteStream(targetPath));
+}
+
+function getPathToFile(release: Release) {
+	const datePath = release.date.toISOString().split('T', 2)[0];
+	return resolve(join(env.DATA_DIR, 'repository', datePath, `${release.id}.zip`));
 }
 
 export { type NewRelease, type Release, createRelease };
