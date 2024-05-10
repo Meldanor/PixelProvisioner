bee6917eda205aaa0b1ce6278d8be38851b2056f
Kilian Gärtner <contact@kilian-gaertner.dev>
2024-05-10 21:07:20 +0200

    add (api): hash the uploaded file with sha1

--- src/lib/server/releases/service.ts
+++ src/lib/server/releases/service.ts
@@ -1,7 +1,9 @@
 import { env } from '$env/dynamic/private';
 import { getDb } from '$lib/server/database/service';
+import { createHash, randomUUID } from 'crypto';
 import { createWriteStream } from 'fs';
 import { mkdir } from 'fs/promises';
+import { pipeline } from 'stream/promises';
 import { dirname, join, resolve } from 'path';
 import { Readable } from 'stream';
 
@@ -15,7 +17,8 @@ interface NewRelease {
 }
 
 interface Release {
-	id: string;
+	_id: string;
+	sha1: string;
 	date: Date;
 	environment: {
 		operatingSystem: string;
@@ -25,34 +28,54 @@ interface Release {
 }
 
 async function createRelease(newRelease: NewRelease): Promise<Release> {
-	const document = {
+	const id = randomUUID().replaceAll('-', '');
+
+	const date = new Date();
+	const hash = await storeAndHashFile(id, date, newRelease.file);
+
+	const release: Release = {
+		_id: id,
 		date: new Date(),
 		environment: newRelease.environment,
-		type: newRelease.type
+		type: newRelease.type,
+		sha1: hash
 	};
-	const newDocument = await getDb().insertAsync(document);
-	const release = {
-		id: newDocument._id,
-		...document
-	};
-	await storeFile(release, newRelease.file);
+
+	await getDb().insertAsync(release);
 	return release;
 }
 
-async function storeFile(release: Release, file: File) {
-	const targetPath = getPathToFile(release);
+async function storeAndHashFile(id: string, date: Date, file: File): Promise<string> {
+	const targetPath = getPathToFile(id, date);
 	await mkdir(dirname(targetPath), { recursive: true });
 
-	// The typing is wrong because it does not support generics in its typing - the function is working
+	const targetStream = createWriteStream(targetPath);
+
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore ts 2551
+	// @ts-ignore
 	const readable = Readable.fromWeb(file.stream());
-	readable.pipe(createWriteStream(targetPath));
+
+	// Create the hash of the file at the same time as copying the file
+	// This should avoid reading the file twice
+	let hash: string = '';
+	const hashPromise = pipeline(
+		readable,
+		createHash('sha1').setEncoding('hex'),
+		async (source: AsyncIterable<string>) => {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			hash = (await source.toArray())[0];
+		}
+	);
+
+	const copyPromise = pipeline(readable, targetStream);
+	await Promise.all([hashPromise, copyPromise]);
+	return hash;
 }
 
-function getPathToFile(release: Release) {
-	const datePath = release.date.toISOString().split('T', 2)[0];
-	return resolve(join(env.DATA_DIR, 'repository', datePath, `${release.id}.zip`));
+function getPathToFile(id: string, date: Date) {
+	const datePath = date.toISOString().split('T', 2)[0];
+	return resolve(join(env.DATA_DIR, 'repository', datePath, `${id}.zip`));
 }
 
 export { type NewRelease, type Release, createRelease };
